f1901dfca24763e7067672e0ecc85a25fbcfab20
MKHG Lab <mkhglab@gmail.com>
2019-08-07 13:49:53 +0600

    Initial Commit

---
+++ MKQRScanner/MKQRScanner.h
@@ -0,0 +1,19 @@
+//
+//  MKQRScanner.h
+//  MKQRScanner
+//
+//  Created by Md. Kamrul Hasan on 8/7/19.
+//  Copyright © 2019 MKHG Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKQRScanner.
+FOUNDATION_EXPORT double MKQRScannerVersionNumber;
+
+//! Project version string for MKQRScanner.
+FOUNDATION_EXPORT const unsigned char MKQRScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKQRScanner/PublicHeader.h>
+
+
